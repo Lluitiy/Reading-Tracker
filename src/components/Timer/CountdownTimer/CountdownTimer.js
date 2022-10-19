ebfc4eb4cad66d243e287e32e1bfacff1538a869
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-10-19 22:58:04 +0300

    all that remains is add logic

--- src/components/Timer/CountdownTimer/CountdownTimer.js
+++ src/components/Timer/CountdownTimer/CountdownTimer.js
@@ -4,23 +4,6 @@ import { useCountdown } from '../../../Hooks/useCountdown/useCountdown';
 
 import { Wrap, Counter, Title, Dots } from './CountdownTimer.styled.jsx';
 
-const ShowCounter = ({ days, hours, minutes, seconds, title }) => {
-	return (
-		<Wrap>
-			<Title>{title}</Title>
-			<Counter>
-				<DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
-				<Dots>:</Dots>
-				<DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
-				<Dots>:</Dots>
-				<DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
-				<Dots>:</Dots>
-				<DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-			</Counter>
-		</Wrap>
-	);
-};
-
 const CountdownTimer = ({ targetDate, title }) => {
 	const [days, hours, minutes, seconds] = useCountdown(targetDate);
 
@@ -28,13 +11,18 @@ const CountdownTimer = ({ targetDate, title }) => {
 		return <p>TIME IS GONE</p>;
 	} else {
 		return (
-			<ShowCounter
-				days={days}
-				hours={hours}
-				minutes={minutes}
-				seconds={seconds}
-				title={title}
-			/>
+			<Wrap>
+				<Title>{title}</Title>
+				<Counter>
+					<DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
+					<Dots>:</Dots>
+					<DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
+					<Dots>:</Dots>
+					<DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
+					<Dots>:</Dots>
+					<DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
+				</Counter>
+			</Wrap>
 		);
 	}
 };
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -26,3 +26,33 @@ const Timer = () => {
 };
 
 export default Timer;
+
+// !______ Для тестів________
+
+// import Timer from 'components/Timer/Timer';
+// import { useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import { getcurrentPlanning } from 'Redux/Planning/planningOperations';
+
+// const HomePage = () => {
+// 	const dispatch = useDispatch();
+
+// 	// useEffect(() => {
+// 	// 	dispatch(getcurrentPlanning());
+
+// 	// 	// eslint-disable-next-line
+// 	// }, []);
+
+// 	return (
+// 		<div>
+// 			<Timer />
+// 		</div>
+// 	);
+// };
+
+// export default HomePage;
+
+// !___________Mail for tests____________
+
+// simpson@mail.ua
+// Simpson123123
