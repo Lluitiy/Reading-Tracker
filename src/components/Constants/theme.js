ceb90c9afc0d77838f1ee00c4989bbd7f9c07171
Pavlo Vovzhynskyy <pavlovovzhynskyy@gmail.com>
2022-10-19 10:15:21 +0200

    add colors to the theme

--- src/components/Constants/theme.js
+++ src/components/Constants/theme.js
@@ -1,6 +1,15 @@
 export const theme = Object.freeze({
 	colors: {
 		white: '#ffffff',
+		googleBtn: '#F5F7FA',
+		body: '#F6F7FB',
+		textMain: '#242A37',
+		textSecondary: '#898F9F',
 		black: '#000000',
+		icons: '#A6ABB9',
+		bookIcons: '#6D7A8D',
+		trainingGrey: '#B1B5C2',
+		brand: '#FF6B08',
+		transparent: 'transparent',
 	},
 });
