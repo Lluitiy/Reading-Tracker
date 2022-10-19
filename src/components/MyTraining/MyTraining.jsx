b29bcbbcafd9a5117383ed502098b76f972c7dc2
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-10-19 23:09:16 +0300

    did myGoals,traning

---
+++ src/components/MyTraining/MyTraining.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Title } from './MyTraining.styled';
+const MyTraining = () => {
+	return (
+		<div>
+			<Title>My training</Title>
+			<form action="">
+				{' '}
+				<label htmlFor="">
+					<input type="date" value="start" placeholder="start"></input>
+				</label>
+				<label htmlFor="">
+					<input type="date" value="finish" placeholder="finish"></input>
+				</label>
+				<select name="select" placeholder="Choose books from the library ">
+					<option value="value1">Значение 1</option>
+					<option value="value2">Значение 2</option>
+					<option value="value3">Значение 3</option>
+				</select>
+			</form>
+		</div>
+	);
+};
+
+export default MyTraining;
