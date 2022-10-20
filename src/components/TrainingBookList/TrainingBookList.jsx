55a8c34232f71f2072b264165b6077ff1c44975f
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-10-20 21:22:31 +0300

    add Goals

--- src/components/TrainingBookList/TrainingBookList.jsx
+++ src/components/TrainingBookList/TrainingBookList.jsx
@@ -5,18 +5,17 @@ import {
 // import React, { useEffect } from 'react';
 import { getBooks } from 'Redux/Planning/planningSelectors';
 // import { getcurrentPlanning } from 'Redux/Planning/planningOperations';
+import { deletePlanningBook } from 'Redux/Planning/planningOperations';
 import { ReactComponent as BookIcon } from 'Assets/svg/book.svg';
-import {
-	// useDispatch,
-	useSelector,
-} from 'react-redux';
-import { getAccessToken } from 'Redux/Auth/authSelectors';
+import { ReactComponent as TrashIcon } from 'Assets/svg/delete.svg';
+import { useDispatch, useSelector } from 'react-redux';
+// import { getAccessToken } from 'Redux/Auth/authSelectors';
 
 const TrainingBookList = () => {
 	const books = useSelector(getBooks);
-	const token = useSelector(getAccessToken);
-	console.log(token);
-	// const dispatch = useDispatch();
+	// const token = useSelector(getAccessToken);
+
+	const dispatch = useDispatch();
 
 	// useEffect(() => {
 	// 	if (token) {
@@ -33,14 +32,17 @@ const TrainingBookList = () => {
 				<span>Pages</span>
 			</ListHeaders>
 			<ul>
-				{books?.map(({ title, author, publishYear, totalPages, _id }) => (
+				{books?.map(({ title, author, publishYear, pagesTotal, _id }) => (
 					<li key={_id}>
 						<ItemWrapper>
 							<BookIcon fill={{}} width={22} height={17} />
 							<span>{title}</span>
 							<span>{author}</span>
 							<span>{publishYear}</span>
-							<span>{totalPages}</span>
+							<span>{pagesTotal}</span>
+							<span onClick={() => dispatch(deletePlanningBook(_id))}>
+								<TrashIcon fill="black" width={22} height={17} />
+							</span>
 						</ItemWrapper>
 					</li>
 				))}
