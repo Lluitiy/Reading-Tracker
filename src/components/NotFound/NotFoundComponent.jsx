ac17bf39f70fda725d93d084c04f8e482af0b40e
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-10-21 17:19:09 +0300

    Add to Not Found check on login

--- src/components/NotFound/NotFoundComponent.jsx
+++ src/components/NotFound/NotFoundComponent.jsx
@@ -1,3 +1,6 @@
+import { getIsLoggedIn } from 'Redux/Auth/authSelectors';
+import { useSelector } from 'react-redux';
+
 import { Section } from 'components/Common/Common.styled';
 import { Link } from 'react-router-dom';
 import {
@@ -13,20 +16,33 @@ import {
 import NotFoundPicSmall from '../../Assets/png/404-img.png';
 
 const NotFoundComponent = () => {
+	const isLoggedIn = useSelector(getIsLoggedIn);
+
 	return (
 		<Section>
 			<NotFoundWrap>
 				<Img src={NotFoundPicSmall} alt="Not Found" />
 				<Title>We are sorry</Title>
 				<Text>We seem to have lost this page, try one of these instead.</Text>
-				<ButtonWrap>
-					<Link to="/library">
-						<Button>Library</Button>
-					</Link>
-					<Link to="/training">
-						<SecondButton>Training</SecondButton>
-					</Link>
-				</ButtonWrap>
+				{isLoggedIn ? (
+					<ButtonWrap>
+						<Link to="/library">
+							<Button>Library</Button>
+						</Link>
+						<Link to="/training">
+							<SecondButton>Training</SecondButton>
+						</Link>
+					</ButtonWrap>
+				) : (
+					<ButtonWrap>
+						<Link to="/login">
+							<Button>Login</Button>
+						</Link>
+						<Link to="/register">
+							<SecondButton>Registration</SecondButton>
+						</Link>
+					</ButtonWrap>
+				)}
 			</NotFoundWrap>
 		</Section>
 	);
