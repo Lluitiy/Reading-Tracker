f2c9c2b52f3573a072a88dc78efd34d088a7c4e3
Valeriya Druchinina <vchumack@glmedia.org>
2022-10-21 03:52:14 +0300

    add start logic by statistics

--- src/Redux/Planning/planningSlice.js
+++ src/Redux/Planning/planningSlice.js
@@ -4,30 +4,49 @@ import {
 	addReadingPage,
 	getCurrentPlanning,
 } from '../Planning/planningOperations';
+
 const initialState = {
 	books: [],
-	booksId:[],
+	booksId: [],
 	startDate: '',
 	endDate: '',
 	pagesPerDay: null,
 	duration: null,
 	stats: null,
+	isShowStartTraningBtn: false,
+	isShowResults: false,
+	planFact: [],
 };
 
 const planningSlice = createSlice({
 	name: 'planning',
 	initialState,
-	reducers: {},
+	reducers: {
+		showStartTraningBtn(state, { payload }) {
+			state.isShowStartTraningBtn = payload;
+		},
+		showResults(state, { payload }) {
+			state.isShowResults = payload;
+		},
+	},
 	extraReducers: {
 		[startPlanning.fulfilled](state, { payload }) {
 			// state.books = [...state.books,...payload.books]
-			state.books=payload.books
-			state.booksId = payload.books.map(({_id})=>_id)
+			state.books = payload.books;
+			state.booksId = payload.books.map(({ _id }) => _id);
 			state.startDate = payload.startDate;
 			state.endDate = payload.endDate;
 			state.pagesPerDay = payload.pagesPerDay;
 			state.duration = payload.duration;
 			state.stats = payload.stats;
+			state.isShowStartTraningBtn = true;
+			for (let i = 1; i <= payload.duration; i += 1) {
+				state.planFact.push({
+					name: `Day ${i}`,
+					fact: 0,
+					plan: payload.pagesPerDay * i,
+				});
+			}
 		},
 		[addReadingPage.fulfilled](state, { payload }) {
 			state.books = state.books.map(book =>
@@ -43,9 +62,10 @@ const planningSlice = createSlice({
 			state.pagesPerDay = payload.planning.pagesPerDay;
 			state.duration = payload.planning.duration;
 			state.stats = payload.planning.stats;
-			state.booksId = payload.planning.books.map(({_id})=>_id)
+			state.booksId = payload.planning.books.map(({ _id }) => _id);
 		},
 	},
 });
 
 export const planningReducer = planningSlice.reducer;
+export const { showStartTraningBtn, showResults } = planningSlice.actions;
