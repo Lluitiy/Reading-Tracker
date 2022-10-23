668176eea2b534e345e6cd662d8ba47486b6f93e
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-10-23 16:43:37 +0300

    change errror message styles

--- src/Utils/validSchema.js
+++ src/Utils/validSchema.js
@@ -9,7 +9,7 @@ export const userRegisterSchema = yup.object({
 	name: yup.string().min(3).max(30).required(),
 	email: yup.string().email().required(),
 	password: yup.string().min(7).max(14).required(),
-	confirm: yup.string().oneOf([yup.ref('password')], 'passwords do not match'),
+	confirm: yup.string().oneOf([yup.ref('password')], 'passwords do not match').required(),
 });
 
 export const booksFormSchema = yup.object({
