ce28eeaa99dda4f2f2cdd8573a1309601fe3576b
Lluitiy <106203735+Lluitiy@users.noreply.github.com>
2022-10-23 19:54:44 +0000

    Deploying to gh-pages from @ Lluitiy/TeamSlice@86246d5e38d6035612e9a94d05ab679569cf19f7 🚀

---
+++ static/js/90.c2d0b0e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkteam_slice=self.webpackChunkteam_slice||[]).push([[90],{7090:function(e,s,c){c.r(s);var t=c(3242),u=c(1200),n=c(2791),i=c(6434),a=c(3905),f=c(1602),r=c(184);s.default=function(){var e=(0,i.useDispatch)();return(0,n.useEffect)((function(){e((0,a.Dq)())}),[e]),(0,r.jsxs)("div",{children:[(0,r.jsx)(t.Z,{}),(0,r.jsx)(u.Z,{}),(0,r.jsx)(f.Z,{})]})}}}]);
+//# sourceMappingURL=90.c2d0b0e2.chunk.js.map
