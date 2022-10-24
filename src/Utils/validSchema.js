6450c611a3a52dae143a58c71a1609d7b6468218
Reizlina Tanya <97384405+Reizlina@users.noreply.github.com>
2022-10-24 19:13:20 +0300

    fix password length

--- src/Utils/validSchema.js
+++ src/Utils/validSchema.js
@@ -2,19 +2,22 @@ import * as yup from 'yup';
 
 export const userLoginSchema = yup.object({
 	email: yup.string().email().required(),
-	password: yup.string().min(7).max(14).required(),
+	password: yup.string().min(7).max(20).required(),
 });
 
 export const userRegisterSchema = yup.object({
 	name: yup.string().min(3).max(30).required(),
 	email: yup.string().email().required(),
-	password: yup.string().min(7).max(14).required(),
-	confirm: yup.string().oneOf([yup.ref('password')], 'passwords do not match').required(),
+	password: yup.string().min(7).max(20).required(),
+	confirm: yup
+		.string()
+		.oneOf([yup.ref('password')], 'passwords do not match')
+		.required(),
 });
 
 export const booksFormSchema = yup.object({
 	title: yup.string().required(),
-	author: yup.string().required( ),
+	author: yup.string().required(),
 	publishYear: yup.string().min(4).required(),
 	pagesTotal: yup.string().min(1).max(4).required(),
-})
+});
