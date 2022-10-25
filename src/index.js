90e064cce382ade9a783907229ded4d5f854cb7a
Pavlo Vovzhynskyy <pavlovovzhynskyy@gmail.com>
2022-10-25 15:35:21 +0200

    fixed small problem

--- src/index.js
+++ src/index.js
@@ -12,19 +12,19 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from 'components/Header/ThemeSwitcher/ThemeCheker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	// <React.StrictMode>
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<BrowserRouter basename="TeamSlice">
-				<ThemeProvider theme={theme()}>
-					<LanguageContextProvider>
-						<ErrorBoundary>
-							<App />
-						</ErrorBoundary>
-					</LanguageContextProvider>
-				</ThemeProvider>
-			</BrowserRouter>
-		</PersistGate>
-	</Provider>
-	// </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter>
+					<ThemeProvider theme={theme()}>
+						<LanguageContextProvider>
+							<ErrorBoundary>
+								<App />
+							</ErrorBoundary>
+						</LanguageContextProvider>
+					</ThemeProvider>
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>
 );
